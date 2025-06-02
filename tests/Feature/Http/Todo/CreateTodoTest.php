7ac15fd57708fe3bca9fbb081414ff160b7dd30b
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-06-02 19:17:01 -0300

    test: testing title validations for creating a todo

--- tests/Feature/Http/Todo/CreateTodoTest.php
+++ tests/Feature/Http/Todo/CreateTodoTest.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Http\Todo;
+
 use App\Enums\TodoStatusEnum;
-use PHPUnit\Framework\Attributes\Test;
 use App\Models\Todo;
 use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class CreateTodoTest extends TestCase
@@ -25,9 +27,38 @@ class CreateTodoTest extends TestCase
         $this->assertDatabaseHas(Todo::class, [
             'title' => 'Todo Title',
             'description' => 'Todo description',
-            'status' => TodoStatusEnum::Pending
+            'status' => TodoStatusEnum::Pending,
         ]);
+    }
 
+    #[DataProvider('title_validation_provider')]
+    public function test_title_validation_rules(array $input, string $error): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $response = $this->post(route('todos.store', [
+            'title' => $input['title'],
+            'description' => 'todo description',
+        ]));
 
+        $response->assertSessionHasErrors(['title' => $error]);
+    }
+
+    public static function title_validation_provider(): array
+    {
+        return [
+            'missing title' => [
+                'input' => ['title' => null],
+                'error' => 'The title field is required.',
+            ],
+            'short title' => [
+                'input' => ['title' => 'aa'],
+                'error' => 'The title field must be at least 3 characters.',
+            ],
+            'long title' => [
+                'input' => ['title' => str_repeat('a', 256)],
+                'error' => 'The title field must not be greater than 255 characters.',
+            ],
+        ];
     }
 }
