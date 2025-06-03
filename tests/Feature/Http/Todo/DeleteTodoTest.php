22a5dce8429396ab852e107944ad7e7a27ac2328
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-06-02 21:24:17 -0300

    test: making sure that only the owner can soft delete a todo

--- tests/Feature/Http/Todo/DeleteTodoTest.php
+++ tests/Feature/Http/Todo/DeleteTodoTest.php
@@ -30,4 +30,20 @@ class DeleteTodoTest extends TestCase
             'description' => 'todo that will be soft deleted'
         ]);
     }
+    #[Test]
+    public function test_only_the_owner_can_soft_delete_the_todo()
+    {
+        $user = User::factory()->create();
+        $wrongUser = User::factory()->create();
+        $todo = Todo::factory()->for($user)->create([
+            'title' => 'title',
+            'description' => 'todo that will be soft deleted'
+        ]);
+        $this->actingAs($wrongUser);
+
+        $response = $this->delete(route('todos.destroy', $todo));
+        $response->assertStatus(403);
+        $todo->fresh();
+        $this->assertNotSoftDeleted($todo);
+    }
 }
